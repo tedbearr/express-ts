0d41ee3ce6e98097cd3d56ba93af519d4a4c17f9
teddy.nutech <teddy.nutech@gmail.com>
2023-12-12 10:24:41 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -64,7 +64,7 @@ app.use(logger.request);
 app.use(logger.response);
 
 app.get("/", (req: Request, res: Response) => {
-  return res.status(200).json("Welcomee!");
+  return res.status(200).json("Welcomeee!");
 });
 
 app.use("/api/v1", globalParameterRoute, authRoute, menuRoute, roleRoute);
